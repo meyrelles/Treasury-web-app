9b9fb0173513d5654dae9320b20d4baf66650c2f
Meyrelles <nuno.de.meireles@gmail.com>
2018-04-28 13:20:16 +0100

    Change the JSON file path

--- app/models/google_synch2.rb
+++ app/models/google_synch2.rb
@@ -5,7 +5,9 @@ class Google_synch_a
 
   def ws
     begin
-      @session ||= GoogleDrive::Session.from_service_account_key("./app/assets/json_files/client_secret.json")
+      #Need to change when uplaod to webserver
+      @session ||= GoogleDrive::Session.from_service_account_key("c:/Ruby25-x64/app/json_files/client_secret.json")
+      
       @spreadsheet ||= @session.spreadsheet_by_key($user_google_key)
       @worksheet ||= @spreadsheet.worksheet_by_title($user_google_sheet_name)
     rescue Exception => e
