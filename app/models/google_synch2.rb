19421ae4c937485a4dcd79022c0c92a1c2c2c504
Meyrelles <nuno.de.meireles@gmail.com>
2018-06-14 22:35:26 +0100

    Dynamic combobox

--- app/models/google_synch2.rb
+++ app/models/google_synch2.rb
@@ -7,7 +7,7 @@ class Google_synch_a
     begin
       #Need to change when uplaod to webserver
       @session ||= GoogleDrive::Session.from_service_account_key("c:/Ruby25-x64/app/json_files/client_secret.json")
-      
+
       @spreadsheet ||= @session.spreadsheet_by_key($user_google_key)
       @worksheet ||= @spreadsheet.worksheet_by_title($user_google_sheet_name)
     rescue Exception => e
